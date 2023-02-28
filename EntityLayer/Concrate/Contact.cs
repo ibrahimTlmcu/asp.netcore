c74519283b31603a17dbd741f82d62aaadce80ac
İbrahim TULUMCU <ibrahimtulumcu@hotmail.com>
2023-02-28 19:49:35 +0300

    Entity Paketleri Eklendi

---
+++ EntityLayer/Concrate/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrate
+{
+    public class Contact
+    {
+        public int ContactID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Mail { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ EntityLayer/Concrate/Testimonial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrate
+{
+    public class Testimonial
+    {
+
+        public int TestomonialID { get; set; }
+        public string ClientName { get; set; }
+        public string Company { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
