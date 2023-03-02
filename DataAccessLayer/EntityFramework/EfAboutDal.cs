fa3c12f48724e8f3ef1b96d9eed104b24c238d0c
İbrahim TULUMCU <ibrahimtulumcu@hotmail.com>
2023-03-02 21:27:26 +0300

    Generic Yapi devam

---
+++ DataAccessLayer/EntityFramework/EfAboutDal.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Repository;
+using EntityLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EfAboutDal:GenericRepository<About>,IAboutDal
+    {
+
+
+    }
+}
---
+++ DataAccessLayer/EntityFramework/EfExperiencedAL.cs
@@ -0,0 +1,28 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Repository;
+using EntityLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    internal class EfExperiencedAL:GenericRepository<Experince>,IExperinceDal
+    {
+    }
+}
+
+
+
+
+
+//GenericRepository metotlar tutuluyor ekle sil guncelle getir vs icinde bunlari 
+
+//(GenericRepository<T> : IGenericDal<T> where T : class) icinde bu yapi ile tutuyor
+
+//<Experionce> entity almis oluyor
+
+//IExperinceDal icinde metot isimleri var ozellileri tanimli 
+
