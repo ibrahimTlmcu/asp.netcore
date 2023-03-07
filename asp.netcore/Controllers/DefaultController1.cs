43a71602e558221307c65d7d75a0532de188cf42
İbrahim TULUMCU <ibrahimtulumcu@hotmail.com>
2023-03-07 19:47:23 +0300

    Tema Eklendi

---
+++ asp.netcore/Controllers/DefaultController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace asp.netcore.Controllers
+{
+    public class DefaultController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
